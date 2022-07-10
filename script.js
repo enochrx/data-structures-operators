017b01fa380abb0f591030132c292e2870b572f6
enochrx <drreayy@gmail.com>
2022-07-10 23:27:02 +0100

    working with strings 3

--- script.js
+++ script.js
@@ -677,9 +677,54 @@ console.log(mess.repeat(5));
 
 const takeoff = tk => {
   console.log(
-    `There are about ${tk} of planes waiting to takeoff ${'✈️'.repeat(tk)}`
+    `There are about ${tk} planes waiting to takeoff ${'✈️'.repeat(tk)}`
   );
 };
 takeoff(5);
 takeoff(10);
 takeoff(2);
+
+/*//Coding Challenge #4
+Write a program that receives a list of variable names written in underscore_case
+and convert them to camelCase.
+The input will come from a textarea inserted into the DOM (see code below to
+insert the elements), and conversion will happen when the button is pressed.
+Test data (pasted to textarea, including spaces):
+underscore_case
+first_name
+Some_Variable
+calculate_AGE
+delayed_departure
+Should produce this output (5 separate console.log outputs):
+underscoreCase       ✅
+firstName            ✅✅
+someVariable         ✅✅✅
+calculateAge         ✅✅✅✅
+delayedDeparture     ✅✅✅✅✅
+Hints:
+§ Remember which character defines a new line in the textarea 😉
+§ The solution only needs to work for a variable made out of 2 words, like a_b
+§ Start without worrying about the ✅. Tackle that only after you have the variable
+name conversion working 😉
+§ This challenge is difficult on purpose, so start watching the solution in case
+you're stuck. Then pause and continue!
+Afterwards, test with your own test data!
+GOOD LUCK 😀*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+//Solution
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+
+  for (const [i, row] of rows.entries()) {
+    const [firstt, secondd] = row.toLowerCase().trim().split('_');
+    const output = `${firstt}${secondd.replace(
+      secondd[0],
+      secondd[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(25)}${'✅'.repeat(i + 1)}`);
+  }
+});
